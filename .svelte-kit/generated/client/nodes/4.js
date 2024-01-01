a31fd414fc79d83fcfe83c4c520625077e868845
joshshipton <shiptonjosh@gmail.com>
2024-01-01 22:17:13 +0800

    stuff

---
+++ .svelte-kit/generated/client/nodes/4.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/edit/add-new-words/+page.svelte";
---
+++ .svelte-kit/generated/client/nodes/5.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/edit/create-new-post/+page.svelte";
