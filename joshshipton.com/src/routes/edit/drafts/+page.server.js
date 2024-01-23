4c9e2596a25af7ef6909e0a373272f27ae0249c7
joshshipton <shiptonjosh@gmail.com>
2024-01-23 16:30:00 +0800

    fuck i hate this pc

---
+++ joshshipton.com/src/routes/edit/drafts/+page.server.js
@@ -0,0 +1,15 @@
+import { supabase } from "$lib/supabase";
+
+export const load = async () => {
+  const { data: posts, error } = await supabase
+    .from("posts")
+    .select("id, title, date_created, post_link, content_peek ")
+    .eq("is_draft", true)
+    .order("date_created", { ascending: false });
+
+  if (error) throw error;
+
+  return {
+    posts,
+  };
+}
---
+++ joshshipton.com/src/routes/edit/drafts/+page.svelte
@@ -0,0 +1,46 @@
+import { supabase } from "$lib/supabase";
+
+export const load = async () => {
+  const { data: posts, error } = await supabase
+    .from("posts")
+    .select("id, title, date_created, post_link, content_peek ")
+    .eq("is_draft", true)
+    .order("date_created", { ascending: false });
+
+  if (error) throw error;
+
+  return {
+    posts,
+  };
+};
+
+// create a form action
+
+export const actions = {
+  addEmail: async ({ request }) => {
+    try {
+      const formData = await request.formData();
+      const email = formData.get("email").trim();
+      const name = formData.get("name").trim();
+
+
+      console.log(name, email)
+
+      // Simple email validation
+      if (!email.match(/^\S+@\S+\.\S+$/)) {
+        return { error: "Invalid email format" };
+      }
+
+      const emailResponse = await supabase
+        .from("subs")
+        .insert({
+          email: email,
+          name: name,
+        })
+        .single();
+
+      if (emailResponse.error) {
+        console.log(emailResponse.error);
+      }
+
+      /
