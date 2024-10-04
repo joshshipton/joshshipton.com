10ba591eaf3bc697e094441d1d1239c37936476c
joshshipton <shiptonjosh@gmail.com>
2024-10-04 09:56:46 +0800

    got that working, defs took 5 minutes loike i told myslef it would

---
+++ joshshipton.com/src/routes/asdfghjklxcvbnmiuytyrgdfg267351293712653jkghdh/+page.svelte
@@ -0,0 +1,24 @@
+<div class="max-w-md mx-auto p-6">
+    <h1 class="text-2xl font-bold mb-8 text-center">lol</h1>
+    <form method="post" action="?/update" class="flex flex-col">
+ 
+            <label for="quoteContent" >content:</label>
+            <textarea id="quoteContent" class="border-black border-2" name="quoteContent" required 
+                      rows="6" ></textarea>
+
+
+
+            <label for="quoteAuthor">author:</label>
+            <input class="border-black border-2" type="text" id="quoteAuthor" name="quoteAuthor" required />
+
+
+
+            <label for="quoteSource" >source (if any):</label>
+            <input class="border-black border-2" type="text" id="quoteSource" name="quoteSource" />
+
+
+        <button type="submit" class= "my-4">
+            submit
+        </button>
+    </form>
+</div>
