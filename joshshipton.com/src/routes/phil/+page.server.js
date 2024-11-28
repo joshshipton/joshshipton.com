ab20e03427fab6b1ef7c2ca6d97bc86f8f53ecdc
joshshipton <shiptonjosh@gmail.com>
2024-11-28 22:23:48 +0000

    code go brr

---
+++ joshshipton.com/src/routes/phil/+page.server.js
@@ -0,0 +1,31 @@
+import { supabase } from "$lib/supabase";
+
+export const load = async () => {
+  // Fetch posts with "grappling" tag
+  const { data: posts, error } = await supabase
+    .from("posts")
+    .select("*")
+    .eq("is_draft", false)
+    .contains("tags", ["phil"]) // This checks if "tags" array contains "grappling"
+    .order("date_created", { ascending: false })
+    .limit(5);
+
+  if (error) throw error;
+
+  // Fetch popular posts with "grappling" tag
+  const { data: popular_posts, popular_error } = await supabase
+    .from("posts")
+    .select("*")
+    .eq("is_draft", false)
+    .contains("tags", ["phil"]) // This checks if "tags" array contains "grappling"
+    .eq("is_popular", true)
+    .order("date_created", { ascending: false })
+    .limit(3);
+
+  if (popular_error) throw popular_error;
+
+  return {
+    posts,
+    popular_posts
+  };
+};
