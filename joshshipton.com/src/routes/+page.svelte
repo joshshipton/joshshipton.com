dbc9d18822cd0d0d14fb86c9809e4c1742815f4e
joshshipton <shiptonjosh@gmail.com>
2024-11-28 16:13:52 +0000

    code go brr

--- joshshipton.com/src/routes/+page.svelte
+++ joshshipton.com/src/routes/+page.svelte
@@ -49,7 +49,7 @@
 <div>
   <!-- Testing if this will show up when google -->
   <p class="text-3xl font-bold text-center">JOSH SHIPTON INTERNET WEBSITE</p>
-  <p class="text-sm text-center">
+<p class="text-lg leading-relaxed text-gray-700">
     <a href="post/me">i'm josh</a>. i'm an <a href="https://www.instagram.com/hiptossjosh/">athlete</a> and <a href="https://www.github.com/joshshipton">software engineer</a>. i made a
     <a href="post/about">personal website</a>
     because people on the internet told me it was a
